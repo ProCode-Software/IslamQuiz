ddccb144ccc9c26f131686e46935648c0e2d4e5d
Sean Mortoti <mail.procode@gmail.com>
2022-10-29 16:35:54 -0500

    Edit

--- js/questions.js
+++ js/questions.js
@@ -7,4 +7,12 @@ const questions = [
         notes: 'Add 10 with 10 and get 20. It is that simple.',
         points: 20
     },
+    {
+        question: 'Who is the 1-year-old baby in the family?',
+        type: 'multipleChoice',
+        answers: ['Joan', 'Lisa', 'Evan', 'Selasie Jr.'],
+        correct: 0,
+        notes: 'Yeah, you should know',
+        points: 100
+    }
 ]
